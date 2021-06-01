3c165c3adc1c5a83fb1aae7d1bcadf1554f6d7d3
renzoamodiajr <renzoamodiajr@gmail.com>
2021-06-01 20:01:54 +0800

    Change db credentials

--- config/dbcon.php
+++ config/dbcon.php
@@ -2,14 +2,23 @@
 
 class DatabaseConnection{
    
-   private $host = 'localhost';
-   private $dbname = 'parkingmonitoring';
-   private $username = 'root';
-   private $password = '';
+   //Get Heroku ClearDB connection information
+   private $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+   private $cleardb_server = $this->cleardb_url["host"];
+   private $cleardb_username = $this->cleardb_url["user"];
+   private $cleardb_password = $this->cleardb_url["pass"];
+   private $cleardb_db = substr($this->cleardb_url["path"],1);
+   public $active_group = 'default';
+   public $query_builder = TRUE;
+   
+   // private $host = $this->cleardb_server;
+   // private $dbname = $this->cleardb_db;
+   // private $username = $this->cleardb_username;
+   // private $password = $this->cleardb_password;
   
    public function connectDb(){
       try {
-         $pdo = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname."", $this->username, $this->password);
+         $pdo = new PDO("mysql:host=".$this->cleardb_server.";dbname=".$this->cleardb_db."", $this->cleardb_username, $this->cleardb_password);
          $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
          return $pdo;
       } catch (PDOException $e) {
